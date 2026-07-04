ed8012b85d090f82e1c2bd68e37f54a1bd6f3cc8
NethraPerera <mr.esanda2001@gmail.com>
2026-07-04 16:16:35 +0530

    final commit

--- app.js
+++ app.js
@@ -86,44 +86,77 @@
 
 
 
-function add() {
-    let num1 = document.getElementById("txt_input1");
-    //console.log(txtInput.value);
-    let userInput1=num1.value;
-    console.log(userInput1);
-    
-    let num2 = document.getElementById("txt_input2");
-    //console.log(txtInput.value);
-    let userInput2=num2.value;
-    console.log(userInput2);
-    
-    let addition=(Number(userInput1)+Number(userInput2));
-    console.log(addition);
-    
-    let heading = document.getElementById("heading1");
-    heading.innerText = addition;
-}
+// function add() {
+//     let num1 = document.getElementById("txt_input1");
+//     let userInput1=num1.value;
+//     console.log(userInput1);
 
-function sub() {
-    let num1 = document.getElementById("txt_input1");
-    //console.log(txtInput.value);
-    let userInput1=num1.value;
-    console.log(userInput1);
+//     let num2 = document.getElementById("txt_input2");
+//     let userInput2=num2.value;
+//     console.log(userInput2);
+
+//     let addition=(Number(userInput1)+Number(userInput2));
+//     console.log(addition);
+
+//     let heading = document.getElementById("heading1");
+//     heading.innerText = addition;
+// }
+
+// function sub() {
+//     let num1 = document.getElementById("txt_input1");
+//     let userInput1=num1.value;
+//     console.log(userInput1);
+
+//     let num2 = document.getElementById("txt_input2");
+//     let userInput2=num2.value;
+//     console.log(userInput2);
+
+//     let substraction=0;
+//     if (userInput1>userInput2) {
+//        substraction=(Number(userInput1)-Number(userInput2)); 
+//     }else{
+//         substraction=(Number(userInput2)-Number(userInput1)); 
+//     }
+//     console.log(substraction);
+
+//     let heading = document.getElementById("heading1");
+//     heading.innerText = substraction;
+//}
+
+//      ////////////////////////////////////////////////////////////////
+
+// let customerList=[];
+
+// function btnAddCustomerOnAction() {
+//     let txtName = document.getElementById("txtName").value;
+//     let txtAddress = document.getElementById("txtAddress").value;
+//     let txtAge = document.getElementById("txtAge").value;
+//     let txtEmail = document.getElementById("txtEmail").value;
+//     let txtSalary = document.getElementById("txtSalary").value;
+
+//     let customer = {
+//         name: txtName,
+//         address: txtAddress,
+//         age: txtAge,
+//         email: txtEmail,
+//         salary: txtSalary
+//     }
+
+//     customerList.push(customer);
+//     console.log(customerList);
     
-    let num2 = document.getElementById("txt_input2");
-    //console.log(txtInput.value);
-    let userInput2=num2.value;
-    console.log(userInput2);
     
-    let substraction=0;
-    if (userInput1>userInput2) {
-       substraction=(Number(userInput1)-Number(userInput2)); 
-    }else{
-        substraction=(Number(userInput2)-Number(userInput1)); 
-    }
-    // let substraction=(Number(userInput1)-Number(userInput2));
-    console.log(substraction);
+// }
+let customerList=[];
+function loadTable(){
+    let tblCustomers=document.getElementById("tblCustomers");
+    tblCustomers.innerHTML+=`<tr>
+            <td>Saman</td>
+            <td>Walana</td>
+            <td>15</td>
+            <td>saman@email.com</td>
+            <td>750000</td>
+        </tr>`
+    console.log(tblCustomers);
     
-    let heading = document.getElementById("heading1");
-    heading.innerText = substraction;
 }
