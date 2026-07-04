58214ae0fad683ff50b2dc241178b0d27afd4a9e
NethraPerera <mr.esanda2001@gmail.com>
2026-07-04 10:12:01 +0530

    objects in js

---
+++ app.js
@@ -0,0 +1,31 @@
+// class customer{
+//     name;
+//     address;
+//     age;
+
+//     constructor(){
+//         this.name="aaa";
+//         this.address="sss";
+//         this.age=10;
+//     }
+// }
+
+cust1={
+    name :"Nethra",
+    address: "Gampaha",
+    age :12,
+    items:["item1","item2","item3"],
+    father:{
+        name:"ajith",
+        address:"gampaha",
+        age:50
+    }
+}
+console.log(cust1.name);
+console.log(cust1.address);
+console.log(cust1.age);
+console.log(cust1.items);
+console.log(cust1.items[1]);
+console.log(cust1.father);
+console.log(cust1.father.age);
+
