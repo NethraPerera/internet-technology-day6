dcd3142f5fc0d110020b882f9fc9114310be3ced
NethraPerera <mr.esanda2001@gmail.com>
2026-07-06 19:02:24 +0530

    recall 1

---
+++ app_note.js
@@ -0,0 +1,12 @@
+let orders={
+    orderId:"or001",
+    orderItem:"rice cooker",
+    orderDate:"2001-10-10",
+    price:2000,
+    customer:{
+        name:"Nethra",
+        age:"24",
+        gender:"male"
+    }
+}
+console.log(orders);
